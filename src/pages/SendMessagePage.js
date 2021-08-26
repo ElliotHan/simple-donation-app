28e8ed8515af0b0bb4390d50d43b16f028627786
elliot <elliot.han@groundx.xyz>
2021-08-26 18:06:13 +0900

    add send message function

--- src/pages/SendMessagePage.js
+++ src/pages/SendMessagePage.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { prepare, request, getResult } from 'klip-sdk'
+import Spinner from '../components/Spinner';
 
 export default function SendMessagePage() {
+  const [SEND_REQUEST, SHOW_LOADING, SHOW_RESULT] = [1, 2, 3]
+
+  const [step, setStep] = useState(SEND_REQUEST)
+  const [message, setMessage] = useState("")
+
+
+  const sendPrepareRequest = async () => {
+    const bappName = '후원하기 앱'
+    const to = '0x2200164c4d78bc4e5e3CCAFA3bc7E006d345f3C0'
+    const value = '0'
+    const abi = JSON.stringify(
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "message",
+            "type": "string"
+          }
+        ],
+        "name": "writeMessage",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+    )
+    const params = `[\"${message}\"]`
+    const res = await prepare.executeContract({ bappName, to, value, abi, params })
+    console.log(res.request_key);
+
+    if (res.request_key) {
+      setStep(SHOW_LOADING)
+      request(res.request_key)
+      startPollingResult(res.request_key)
+    }
+  }
+
+  const startPollingResult = (requestKey) => {
+    const id = setInterval(async () => {
+      const res = await getResult(requestKey)
+      if (res.status === 'completed') {
+        clearTimeout(id)
+        setStep(SHOW_RESULT)
+      }
+    }, 1000);
+  }
+
   return (
     <div>
-      SendMessage
+      {step === SEND_REQUEST &&
+        (<>
+          <div>후원 메시지를 적어주세요</div>
+          <input type="text" onChange={(e) => setMessage(e.target.value)} />
+          <button
+            onClick={sendPrepareRequest}
+          >후원 메시지 보내기</button>
+        </>)}
+      {step === SHOW_LOADING && (
+        <Spinner />
+      )}
+      {step === SHOW_RESULT && (
+        <div> 후원 메시지를 보냈습니다!</div>
+      )}
     </div>
   )
 }
