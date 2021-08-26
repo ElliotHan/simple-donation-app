8b9a7f14d5b73f15c9b1c447536e07e701621e9b
elliot <elliot.han@groundx.xyz>
2021-08-26 17:00:40 +0900

    add spinner

---
+++ src/components/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Spinner.scss';
+
+export default function Spinner() {
+  return (
+    <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  )
+}
