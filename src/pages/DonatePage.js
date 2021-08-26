8ab877212701b031676f89c66bd2d4125804ecaa
elliot <elliot.han@groundx.xyz>
2021-08-26 18:05:59 +0900

    add donate function

--- src/pages/DonatePage.js
+++ src/pages/DonatePage.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { prepare, request, getResult } from 'klip-sdk'
+import Spinner from '../components/Spinner';
 
 export default function DonatePage() {
+  const [SEND_REQUEST, SHOW_LOADING, SHOW_RESULT] = [1, 2, 3]
+
+  const [step, setStep] = useState(SEND_REQUEST)
+
+  const sendPrepareRequest = async () => {
+    const bappName = '후원하기 앱'
+    const to = '0x61f9e95Fa5DC71218Bb4FEbDa2Bb35522EAD7Dd3'
+    const amount = '1'
+    const res = await prepare.sendKLAY({ bappName, to, amount })
+
+    if (res.request_key) {
+      setStep(SHOW_LOADING)
+      request(res.request_key)
+      startPollingResult(res.request_key)
+    }
+  }
+
+  const startPollingResult = (requestKey) => {
+    const id = setInterval(async () => {
+      const res = await getResult(requestKey)
+      if (res.status === 'completed') {
+        clearTimeout(id)
+        setStep(SHOW_RESULT)
+      }
+    }, 1000);
+  }
+
   return (
     <div>
-      DonatePage
+      {step === SEND_REQUEST &&
+        (<>
+          <div>1KLAY를 아래 주소로 후원</div>
+          <button
+            onClick={sendPrepareRequest}
+          >후원하기</button>
+        </>)}
+      {step === SHOW_LOADING && (
+        <Spinner />
+      )}
+      {step === SHOW_RESULT && (
+        <div> 후원이 완료되었습니다.</div>
+      )}
     </div>
   )
 }
